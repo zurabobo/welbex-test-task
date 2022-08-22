619332745e830069ad747f2a52842fc0abdec612
Zura <zuramariwedding@gmail.com>
2022-08-22 16:58:45 +0400

    heroku

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cors());
+app.use(cors())
 
 app.get('/', (req, res) => {
     db.select('*')
