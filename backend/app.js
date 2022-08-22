c1822edad6fd59a025fa2b88ad509c3357cb0d3e
Zura <zuramariwedding@gmail.com>
2022-08-22 17:47:44 +0400

    heroku

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,17 @@ const cors = require('cors');
 const knex = require('knex');
 const db = knex({
     client: 'pg',
-    connection: {
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE
-    },
+    connection: process.env.DATABASE_URL
 });
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         host: process.env.DATABASE_HOST,
+//         user: process.env.DATABASE_USERNAME,
+//         password: process.env.DATABASE_PASSWORD,
+//         database: process.env.DATABASE
+//     },
+// });
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
