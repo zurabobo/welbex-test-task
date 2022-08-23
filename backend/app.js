b60fc74f1dfced6fe5d18f0ef0c26001a6a47676
Zura <zuramariwedding@gmail.com>
2022-08-23 12:58:32 +0400

    heroku

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
-const db = knex({
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+var connectionString = "postgres://mbjbpngwbtmcxj:81a003beaf1c763b47e850f0d95d768efb15495cb0db256ce41237736a080ae5@ec2-34-199-68-114.compute-1.amazonaws.com:5432/dd8hrerjqrq03a"
+
+pg.connect(connectionString, function(err, client, done) {
+   client.query('SELECT * FROM main', function(err, result) {
+      done();
+      if(err) return console.error(err);
+      console.log(result.rows);
+   });
 });
+// const db = knex({
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL,
+//     ssl: true,
+//     extra: {
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+// });
 // const db = knex({
 //     client: 'pg',
 //     connection: {
