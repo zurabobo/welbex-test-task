61b4b7e52d8631a1973f507e6b1436a3678aae0a
Zura <zuramariwedding@gmail.com>
2022-09-02 12:34:52 +0400

    heroku

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,12 @@ const pg = require('pg');
 
 const db = knex({
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE
+    },
     searchPath: ['knex', 'public'],
 });
 // const db = knex({
