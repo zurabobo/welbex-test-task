3cf3f86514670ec72c23c0e3816fc78a4209898c
Zura <zuramariwedding@gmail.com>
2022-09-02 13:35:44 +0400

    deploy frontend on netlify

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,4 @@
-export const API_URL = 'http://localhost:5000';
+// export const API_URL = 'http://localhost:5000';
+export const API_URL = 'https://welbex-testtask.herokuapp.com/';
 export const QUANTITY_DATAS_TO_RENDER = 5;
 export const QUANTIITY = 0;
