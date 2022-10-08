f80668daa82fff7a7dd85d0afbd9efe5e69be8f5
Zura <zuramariwedding@gmail.com>
2022-10-08 13:20:43 +0400

    prelaoder

--- frontend/src/components/Preloader/Preloader.js
+++ frontend/src/components/Preloader/Preloader.js
@@ -1,80 +1,65 @@
-// import React from "react";
-// import "./Preloader.css";
-// import { Table } from "react-bootstrap";
-
-// const Preloader = () => {
-//   return (
-//     <Table
-//       striped
-//       bordered
-//       className="border-color text-center p-3 mx-auto mt-5 mb-4"
-//     >
-//       <thead>
-//         <tr>
-//           <th>Дата</th>
-//           <th>
-//             Название
-//             <button className="sort-button"></button>
-//           </th>
-//           <th>
-//             Количество
-//             <button className="sort-button"></button>
-//           </th>
-//           <th>
-//             Расстояние
-//             <button className="sort-button" data-type="distance"></button>
-//           </th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         <tr className="shimmer">
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//         </tr>
-//         <tr className="shimmer">
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//         </tr>
-//         <tr className="shimmer">
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//         </tr>
-//         <tr className="shimmer">
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//         </tr>
-//         <tr className="shimmer">
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//           <td></td>
-//         </tr>
-//       </tbody>
-//     </Table>
-//   );
-// };
-
-// export default Preloader;
-
-import React from 'react'
-import './Preloader.css'
+import React from "react";
+import "./Preloader.css";
+import { Table } from "react-bootstrap";
 
 const Preloader = () => {
-    return (
-        <div className="preloader">
-            <div className="preloader__container">
-                <span className="preloader__round"></span>
-            </div>
-        </div>
-    )
+  return (
+    <Table
+      striped
+      bordered
+      className="border-color text-center p-3 mx-auto mt-5 mb-4"
+    >
+      <thead>
+        <tr>
+          <th>Дата</th>
+          <th>
+            Название
+            <button className="sort-button"></button>
+          </th>
+          <th>
+            Количество
+            <button className="sort-button"></button>
+          </th>
+          <th>
+            Расстояние
+            <button className="sort-button" data-type="distance"></button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="shimmer">
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr className="shimmer">
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr className="shimmer">
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr className="shimmer">
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr className="shimmer">
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </Table>
+  );
 };
 
-export default Preloader
+export default Preloader;
