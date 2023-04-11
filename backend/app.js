8de2003ccdf3eabcb6d4691a50573c18754d7b95
Zura <zuramariwedding@gmail.com>
2023-04-11 13:00:47 +0400

    deployment to azure

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,25 @@ const cors = require('cors');
 const knex = require('knex');
 const pg = require('pg');
 
-// const db = new pg.Client(process.env.DATABASE_URL);
-// db.connect();
-// const query = 'SELECT * FROM main';
-// db.query(qyery, (req,res) => {
-//     console.log(res)
-// })
+const options = {
+    origin: [
+      "http://localhost:8080",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+  "https://welbex-testtask.netlify.app"
+      // 'https://movies-explorer-api-yv7u.onrender.com',
+    //   "https://movies-explorer-frontend.netlify.app",
+      // 'https://movies-explorer.zb.nomoredomains.rocks',
+      // 'https://api.movies-explorer.zb.nomoredomains.rocks',
+    ],
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "origin", "Authorization"],
+    credentials: true,
+  };
+  
 const db = knex({
     client: 'pg',
     connection: {
@@ -23,7 +36,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
+app.use("*", cors(options));
 
 app.get('/', (req, res) => {
     db.select('*')
